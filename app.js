e3efb0a38676b4bfaf71f7e9779773b4543a458d
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-06-13 13:58:17 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+    console.log('Weather is sunny!');
+});
+ 
+
+client.login(process.env.WEATHER_NETWORK);
