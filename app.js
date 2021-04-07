00bd06b0e1b1ab2f4f7c4d59b5161344056c7749
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2021-04-07 10:51:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
+    
     console.log('Weather is sunny!');
 });
+//
+
 client.login(process.env.WEATHER_NETWORK);
